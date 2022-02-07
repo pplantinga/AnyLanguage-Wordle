bd6482a0adef57ebe8af2b573b38642f4760f137
calvinballing <calvinballing@gmail.com>
2022-02-07 17:15:17 -0500

    Update example words to humor-related terms

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -15,36 +15,36 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
+        <Cell value="J" status="correct" />
         <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="K" />
+        <Cell value="E" />
+        <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500">
-        The letter L is in the word but in the wrong spot.
+        The letter J is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
+        <Cell value="L" />
         <Cell value="A" />
+        <Cell value="U" status="present" />
         <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="H" />
       </div>
       <p className="text-sm text-gray-500">
-        The letter U is not in the word in any spot.
+        The letter U is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="F" />
+        <Cell value="U" />
+        <Cell value="N" />
+        <Cell value="N" />
+        <Cell value="Y" status="absent" />
+      </div>
+      <p className="text-sm text-gray-500">
+        The letter Y is not in the word in any spot.
       </p>
     </BaseModal>
   )
