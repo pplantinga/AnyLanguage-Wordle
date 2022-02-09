ce13595cceaa09664a920d4209a4a9a84d5f60f9
calvinballing <calvinballing@gmail.com>
2022-02-08 22:18:24 -0500

    make language joke-specific

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,7 +11,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
         Guess the CHORTLE in 4 tries. After each guess, the color of the tiles
-        will change to show how close your guess was to the word.
+        will change to show how close your guess was to the joke's punchline.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -22,7 +22,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500">
-        The letter J is in the word and in the correct spot.
+        The letter J is in the punchline and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -33,7 +33,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="H" />
       </div>
       <p className="text-sm text-gray-500">
-        The letter U is in the word but in the wrong spot.
+        The letter U is in the punchline but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -44,7 +44,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="Y" status="absent" />
       </div>
       <p className="text-sm text-gray-500">
-        The letter Y is not in the word in any spot.
+        The letter Y is not in the punchline in any spot.
       </p>
     </BaseModal>
   )
